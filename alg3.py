2d676f39844b2c08567fb66eff95aa7c7ef4d4a7
Manuel <unimanuel@yandex.com>
2023-12-01 23:30:35 +0100

    - haar wavelet tests - clipping (less edge sharpness)

--- alg3.py
+++ alg3.py
@@ -10,13 +10,13 @@ class Alg3WaveletTest(object):
         print("Algorithm3 (wavelet decomposition) starting.")
         family = 'cmor'
         waveletchoice = 'haar'
-        for family in pywt.families():
+        for family in ['haar']: #pywt.families():
             print('family', family)
             if family == 'gaus':
                 print('continuing')
                 continue
             for waveletchoice in pywt.wavelist(family):
-            # for waveletchoice in ['haar']:
+            # for waveletchoice in ['haar']:^
                 print(pywt.families())
                 print(pywt.wavelist(family))
                 print('waveletchoice', waveletchoice)
@@ -34,7 +34,35 @@ class Alg3WaveletTest(object):
                 decomp2 = pywt.dwt2(firstimg[:,:,1], waveletchoice, mode='per')
                 decomp3 = pywt.dwt2(firstimg[:,:,2], waveletchoice, mode='per')
                 recompimg = np.zeros_like(firstimg)
-                # print(recompimg.shape)
+
+                LL1, (LH1, HL1, HH1) = decomp1
+                LL2, (LH2, HL2, HH2) = decomp2
+                LL3, (LH3, HL3, HH3) = decomp3
+                print(LH1)
+                print('Processing')
+                # LH1[0:100,:] = LH1[100:200,:]
+                # HL1[0:100,:] = HL1[100:200,:]
+                # HH1[0:100,:] = HH1[100:200,:]
+                print('ma',np.max(LH1))
+                LH1 = np.clip(LH1, -1, 1)
+                HL1 = np.clip(HL1, -1, 1)
+                HH1 = np.clip(HH1, -1, 1)
+                LH2 = np.clip(LH2, -1, 1)
+                HL2 = np.clip(HL2, -1, 1)
+                HH2 = np.clip(HH2, -1, 1)
+                LH3 = np.clip(LH3, -1, 1)
+                HL3 = np.clip(HL3, -1, 1)
+                HH3 = np.clip(HH3, -1, 1)
+                # decomp3[1] = np.clip(decomp3[1], -100, 100)
+                print(LH1)
+                decomp1 = LL1, (LH1, HL1, HH1)
+                decomp2 = LL2, (LH2, HL2, HH2)
+                decomp3 = LL3, (LH3, HL3, HH3)
+                # decomp1 = abs(LL1), (abs(LH1), abs(HL1), abs(HH1))
+                # decomp2 = abs(LL2), (abs(LH2), abs(HL2), abs(HH2))
+                # decomp3 = abs(LL3), (abs(LH3), abs(HL3), abs(HH3))
+
+                # print(recompimg.shape)dadssss
                 recompimg[:,:,2] = pywt.idwt2(decomp1, waveletchoice, mode='per')[0:recompimg.shape[0],:]
                 recompimg[:,:,1] = pywt.idwt2(decomp2, waveletchoice, mode='per')[0:recompimg.shape[0],:]
                 recompimg[:,:,0] = pywt.idwt2(decomp3, waveletchoice, mode='per')[0:recompimg.shape[0],:]
@@ -79,4 +107,4 @@ class Alg3WaveletTest(object):
                     print('Decomposition figure saved in ' + figname)
 
         print('FINISHED METHOD. Returning low pass filtered image (smaller size).')
-        return combinedImg * 255
+        return combinedImg * 211
