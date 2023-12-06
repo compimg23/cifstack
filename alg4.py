230a3d8d196f3d605020735d0de592257230f4b8
Manuel <unimanuel@yandex.com>
2023-12-06 18:06:32 +0100

    - use gray image for decomposition comparisons.

--- alg4.py
+++ alg4.py
@@ -40,6 +40,7 @@ class Alg4MergeTest(object):
                 decomp1 = pywt.dwt2(firstimg[:,:,0], waveletchoice, mode='per')
                 decomp2 = pywt.dwt2(firstimg[:,:,1], waveletchoice, mode='per')
                 decomp3 = pywt.dwt2(firstimg[:,:,2], waveletchoice, mode='per')
+                newdecompg = np.zeros_like(decomp1[0]), (np.zeros_like(decomp1[1][0]), np.zeros_like(decomp1[1][1]), np.zeros_like(decomp1[1][2]))
                 newdecomp1 = np.zeros_like(decomp1[0]), (np.zeros_like(decomp1[1][0]), np.zeros_like(decomp1[1][1]), np.zeros_like(decomp1[1][2]))
                 newdecomp2 = np.zeros_like(decomp1[0]), (np.zeros_like(decomp1[1][0]), np.zeros_like(decomp1[1][1]), np.zeros_like(decomp1[1][2]))
                 newdecomp3 = np.zeros_like(decomp1[0]), (np.zeros_like(decomp1[1][0]), np.zeros_like(decomp1[1][1]), np.zeros_like(decomp1[1][2]))
@@ -47,23 +48,39 @@ class Alg4MergeTest(object):
                     currimg = img_mats[j]
                     print("firstimg shape", firstimg.shape)
                     print("Running wavelet decomposition.")
+
+                    imggray = cv2.cvtColor(currimg, cv2.COLOR_BGR2GRAY)
+                    
+                    decompgray = pywt.dwt2(imggray, waveletchoice, mode='per')
+
                     decomp1 = pywt.dwt2(currimg[:,:,0], waveletchoice, mode='per')
                     decomp2 = pywt.dwt2(currimg[:,:,1], waveletchoice, mode='per')
                     decomp3 = pywt.dwt2(currimg[:,:,2], waveletchoice, mode='per')
+
+                    LLg, (LHg, HLg, HHg) = decompgray
 
                     LL1, (LH1, HL1, HH1) = decomp1
                     LL2, (LH2, HL2, HH2) = decomp2
                     LL3, (LH3, HL3, HH3) = decomp3
                     # print(LH1)
                     print('Processing')
+
+                    decompgray = LLg, (LHg, HLg, HHg)
+
                     decomp1 = LL1, (LH1, HL1, HH1)
                     decomp2 = LL2, (LH2, HL2, HH2)
                     decomp3 = LL3, (LH3, HL3, HH3)
-                    newdecomp1 = self.combine_decomps(newdecomp1, decomp1)
-                    newdecomp2 = self.combine_decomps(newdecomp2, decomp2)
-                    newdecomp3 = self.combine_decomps(newdecomp3, decomp3)
+
+                    # newdecompg = self.combine_decomps(newdecompg, decompgray)
+
+                    newdecomp1, newdecompg = self.combine_decomps_gray(newdecomp1, newdecompg, decomp1, decompgray)
+                    newdecomp2, newdecompg = self.combine_decomps_gray(newdecomp2, newdecompg, decomp2, decompgray)
+                    newdecomp3, newdecompg = self.combine_decomps_gray(newdecomp3, newdecompg, decomp3, decompgray)
                     
                     recompimg = np.zeros_like(currimg)
+                    recompimggray = np.zeros_like(imggray)
+
+                    recompimggray[:,:] = pywt.idwt2(newdecompg, waveletchoice, mode='per')[0:recompimg.shape[0],0:recompimg.shape[1]]
                     recompimg[:,:,0] = pywt.idwt2(newdecomp1, waveletchoice, mode='per')[0:recompimg.shape[0],0:recompimg.shape[1]]
                     recompimg[:,:,1] = pywt.idwt2(newdecomp2, waveletchoice, mode='per')[0:recompimg.shape[0],0:recompimg.shape[1]]
                     recompimg[:,:,2] = pywt.idwt2(newdecomp3, waveletchoice, mode='per')[0:recompimg.shape[0],0:recompimg.shape[1]]
@@ -74,6 +91,12 @@ class Alg4MergeTest(object):
                     # im1.save(recompname)
                     cv2.imwrite(recompname, recompimg)
                     print('Recomposition saved in ' + recompname)
+
+                    recompgrayname = 'OutputFolder/dwt_' + waveletchoice + '_recomp_gray' + str(j) + '.jpg'
+                    print('Saving recomposition')
+                    # im1.save(recompname)
+                    cv2.imwrite(recompgrayname, recompimggray)
+                    print('Recomposition gray saved in ' + recompgrayname)
                     
                     combinedImg = np.zeros((decomp1[0].shape[0],decomp1[0].shape[1],3))
                     k = 0
@@ -124,3 +147,30 @@ class Alg4MergeTest(object):
             newdecompx[1][2], currdecomp[1][2])
         newdecompx = newdecompx0, (newdecompx10, newdecompx11, newdecompx12)
         return newdecompx
+
+    def combine_decomps_gray(self, newdecompx, newdecompg, currdecomp, currdecompg):
+        # Boolean matrix tells us for each pixel which image has the three high-pass subband pixels with greatest total abs value.
+        boolMat = np.abs(newdecompg[1][0]) + np.abs(newdecompg[1][1]) + np.abs(newdecompg[1][2]) > \
+                         np.abs(currdecompg[1][0]) + np.abs(currdecompg[1][1]) + np.abs(currdecompg[1][2])
+        # copy pixel values for all four subbands according to boolean matrix.
+        newdecompx0 = np.where(boolMat,
+                newdecompx[0], currdecomp[0])
+        newdecompx10 = np.where(boolMat,
+            newdecompx[1][0], currdecomp[1][0])
+        newdecompx11 = np.where(boolMat,
+            newdecompx[1][1], currdecomp[1][1])
+        newdecompx12 = np.where(boolMat,
+            newdecompx[1][2], currdecomp[1][2])
+        newdecompx = newdecompx0, (newdecompx10, newdecompx11, newdecompx12)
+
+        newdecompg0 = np.where(boolMat,
+                newdecompg[0], currdecompg[0])
+        newdecompg10 = np.where(boolMat,
+            newdecompg[1][0], currdecompg[1][0])
+        newdecompg11 = np.where(boolMat,
+            newdecompg[1][1], currdecompg[1][1])
+        newdecompg12 = np.where(boolMat,
+            newdecompg[1][2], currdecompg[1][2])
+        newdecompg = newdecompg0, (newdecompg10, newdecompg11, newdecompg12)
+
+        return newdecompx, newdecompg
